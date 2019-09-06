3f7555bf1f96f26aca2d7527f0b44601f86848c4
Unknown <me@dennisschmidt.net>
2019-09-06 15:42:43 +0200

    Added qualigruppen, compressed images

--- js/mapping.js
+++ js/mapping.js
@@ -25,7 +25,7 @@ function getNationality(driver) {
   else if (name.includes("abt")) { return "de"; }
   else if (name.includes("lotterer")) { return "de"; }
   else if (name.includes("wehrlein")) { return "de"; }
-  
+
   else if (name.includes("lopez")) { return "ar"; }
 
   else if (name.includes("dillmann")) { return "fr"; }
@@ -53,8 +53,7 @@ function getTeam(driver) {
   else if (name.includes("vandoorne")) { return "mercedes"; }
   else if (name.includes("paffett")) { return "mercedes"; }
 
-  else if (name.includes("günther")) { return "dragon"; }
-  else if (name.includes("nasr")) { return "dragon"; }
+  else if (name.includes("hartley")) { return "dragon"; }
   else if (name.includes("lopez")) { return "dragon"; }
 
   else if (name.includes("dillmann")) { return "nio"; }
@@ -70,9 +69,10 @@ function getTeam(driver) {
   else if (name.includes("buemi")) { return "nissan"; }
 
   else if (name.includes("vergne")) { return "techeetah"; }
+  else if (name.includes("da costa")) { return "techeetah"; }
 
+  else if (name.includes("günther")) { return "bmw"; }
   else if (name.includes("sims")) { return "bmw"; }
-  else if (name.includes("da costa")) { return "bmw"; }
 
   else if (name.includes("ambrosio")) { return "mahindra"; }
   else if (name.includes("rosenqvist")) { return "mahindra"; }
@@ -94,6 +94,25 @@ function getTeam(driver) {
   else if (name.includes("hwa")) { return "hwa"; }
   else if (name.includes("mercedes")) { return "mercedes"; }
   else if (name.includes("porsche")) { return "porsche"; }
+}
+
+function getFullTeam(driver) {
+  var name = driver.toLowerCase();
+  var team = getTeam(driver);
+
+  if (team.includes("techeetah")) { return "DS Techeetah"; }
+  else if (team.includes("mahindra")) { return "Mahindra Racing"; }
+  else if (team.includes("virgin")) { return "Envision Virgin Racing"; }
+  else if (team.includes("nissan")) { return "Nissan e.Dams"; }
+  else if (team.includes("bmw")) { return "BMW i Andretti Motorsport"; }
+  else if (team.includes("audi")) { return "Audi Sport Abt Schaeffler"; }
+  else if (team.includes("nio")) { return "NIO Formula E Team"; }
+  else if (team.includes("venturi")) { return "Venturi Formula E Team"; }
+  else if (team.includes("dragon")) { return "GEOX Dragon"; }
+  else if (team.includes("jaguar")) { return "Panasonic Jaguar Racing"; }
+  else if (team.includes("hwa")) { return "HWA Racelab"; }
+  else if (team.includes("mercedes")) { return "Mercedes EQ Formula E Team"; }
+  else if (team.includes("porsche")) { return "Porsche Formula E Team"; }
 }
 
 function getCountry(city) {
