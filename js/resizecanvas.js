ee9dff9d7d4e1480d7c493c162a109465403cc57
Unknown <me@dennisschmidt.net>
2019-10-02 21:19:31 +0200

    Added stories

--- js/resizecanvas.js
+++ js/resizecanvas.js
@@ -1,8 +1,10 @@
-function resizeCanvas(height) {
+function resizeCanvas(height, scale) {
   var canvas = document.getElementById('canvas');
   var preview = document.getElementById('preview');
   var factor = (preview.offsetWidth - 80) / canvas.offsetWidth;
-  canvas.style.transform = 'scale(' + factor + ')';
+  canvas.style.transform = 'scale(' + (factor * scale) + ')';
 
-  canvas.style.marginBottom = ((height - (height * factor)) * -1) + 'px';
+  canvas.style.marginBottom = ((height - (height * (factor * scale))) * -1) + 'px';
+  canvas.style.marginLeft = 'auto';
+  canvas.style.marginRight = 'auto';
 }
